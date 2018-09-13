0c146b318a3771da16033ea27e2488794b533686
Neil Sood <neilsood@Neils-MacBook-Pro.local>
2018-09-12 19:06:59 -0700

    to do list first commit

---
+++ ToDoList/MainTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainTableViewController.swift
+//  ToDoList
+//
+//  Created by Neil Sood on 9/12/18.
+//  Copyright © 2018 Neil Sood. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
